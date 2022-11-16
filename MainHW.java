a9948c3f6d151b545e957af4c3b2c7ecc4bcd58f
Roye ahronee <117036475+Roye-A@users.noreply.github.com>
2022-11-16 02:48:20 +0200

    עבודת בית תרגיל פונקציות QA 32
    עבודת בית תרגיל פונקציות QA 32

---
+++ MainHW.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+public class MainHW {
+    public static void main(String[] args) {
+
+        //1.פונקציה היא הגדרה של קטע קוד
+        //ושנתשמש בו כשנרצה לשמור ולהריץ את אותן אוסף פקודות
+        //השימוש בה הוא כדי שהתכנות יהיה יותר טוב לעין
+
+        //2.4 סוגי הפונקציות בגאווה הם- פוקנצייה שלא מקבלת פרמטים ואין ערך מוחזר
+        //-שימוש בהדפסה או סידור מערך.
+        //פונקצייה שכן מקבלת פרמטים ומחזירה ערך
+        //-שימוש בפונקציית חישוביות, ביצוע פעולות
+        //והחזרה של true/false מקבל משתנה.
+        //פונקצייה שלא מקבלת פרמטר אבל כן מחזירה ערך
+        //-שימוש בקבלת מידע מהמשתמש או מהמחשב.
+        //פונקצייה שמקבלת פרמטר אבל לא מחזירה ערך
+        //-שימוש בהדפסה ספציפית.
+
+        // 3.יודפס 0 כי הגדרנו ב-main את X מחדש
+//        int x =0;
+//        increment(x);
+//        increment(x);
+//        increment(x);
+//        System.out.println(x);
+
+        //4.א. מה הפונקציה תחזיר עבור הערכים 4 7
+//        System.out.println(sod(7, 4));
+        //ב. מה הפונקציה תחזירה עבור הערכים 3 4
+//        System.out.println(sod(4, 3));
+        //ג. מה הפונקציה תחזיר עבור הערכים 4 3
+//        System.out.println(sod(3,4));
+        //ד. מה מבצעת הפונקציה עבור כל קלט של שני מספרים שלמים
+        //הפונקציה בודקת אם אחד המספריים +1
+        //יהיה שווה למספר השני אם כן true
+
+        //5.כתוב פעולה שמקבלת הפנייה למחרוזת תווים,
+        //ותו בודד.הפעולה תחזיר true אם התו
+        //נמצא במחרוזת, false אחרת
+//        System.out.println(checkIf());
+
+        //6. כתבו פונקציה 4 פונקציות של משחבון +-*/
+        //הפונקציות מקבלות שני מספרים
+        //ומחזירות את התשובה של המשוואה
+//        System.out.println(cal1());
+//        System.out.println(cal2());
+//        System.out.println(cal3());
+//        System.out.println(cal4());
+
+    }
+//    public static void increment(int i){
+//        i = i + 1;
+//    }
+
+//    public static boolean sod(int a,int b){
+//        if (a == b+1)
+//            return true;
+//        if (b == a+1)
+//            return true;
+//        return false;
+//    }
+
+//    public static boolean checkIf() {
+//        Scanner sc1 = new Scanner(System.in);
+//        String write = sc1.nextLine();
+//        if (write.contains("R"))
+//            return true;
+//        return false;
+//    }
+
+//    public static int cal1(){
+//        Scanner sc2 = new Scanner(System.in);
+//        int x = sc2.nextInt(), y = sc2.nextInt();
+//        return (x+y);
+//    }
+//    public static int cal2(){
+//        Scanner sc2 = new Scanner(System.in);
+//        int x = sc2.nextInt(), y = sc2.nextInt();
+//        return (x-y);
+//    }
+//    public static int cal3(){
+//        Scanner sc2 = new Scanner(System.in);
+//        int x = sc2.nextInt(), y = sc2.nextInt();
+//        return (x*y);
+//    }
+//    public static double cal4(){
+//        Scanner sc2 = new Scanner(System.in);
+//        double x = sc2.nextDouble(), y = sc2.nextDouble();
+//        return (x/y);
+//    }
+    
+}
